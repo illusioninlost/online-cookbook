608e48c9e29e0eb635dd7a95aa4836d66dda1432
Richard Chang <rchang9999@yahoo.com>
2018-05-03 17:45:28 +0000

    fixing new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,28 @@
 class RecipesController < ApplicationController
 
   def index
+    @recipes = Recipe.all
   end
 
   def new
+    @recipe = Recipe.new
   end
+
+  def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(
+    :name,
+    :creator,
+    :notes,
+    :special_tools,
+    :approximate_time,
+    :ingredients
+    )
+  end
+
 end
