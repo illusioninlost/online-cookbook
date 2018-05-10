92dd9f46f88278c4c9a19b13e5862c28424e281b
Richard Chang <rchang9999@yahoo.com>
2018-05-10 17:06:15 +0000

    adding comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def new
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  def create
+    @recipe = Recipe.find_by(id: params[:id])
+    @comment = @recipe.comments.create(comments_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+
+  private
+  def comments_params
+    params.require(:comment).permit(:name, :content, :ratings)
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
   has_many :users
-  has_many :comments, :through => :users
+  has_many :comments
 end
