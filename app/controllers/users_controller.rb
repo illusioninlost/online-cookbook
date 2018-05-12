1ffea361404a39675fb9d6912f168d1df274bcfe
Richard Chang <rchang9999@yahoo.com>
2018-05-12 21:03:22 +0000

    adding login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,44 @@
 class UsersController < ApplicationController
 
   def new
+    session.delete("user_id") if session[:user_id]
     @user = User.new
   end
 
   def create
+    if (@user = User.create(user_params))
+    @user.save
+    session[:user_id]=@user.id
+    redirect_to recipes_path
+  else
+    render 'new'
+    end
   end
 
   def login
+    session.delete("user_id") if session[:user_id]
   end
+
+  def in
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id]=@user.id
+      redirect_to recipes_path
+    else
+      render 'login'
+    end
+  end
+
+  def destroy
+    session.delete("user_id")
+    redirect_to recipes_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(
+      :username,
+      :password
+      )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'recipes#index'
 
   get '/login', to: 'users#login', as: 'login'
+  post '/login', to: 'users#in', as: 'in'
   resources :users, only: [:new,:create,:destroy]
   resources :recipes do
     resources :comments
