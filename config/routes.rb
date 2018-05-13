ce8f386fe8193b1c6274402d8fb60fd680d53466
Richard Chang <rchang9999@yahoo.com>
2018-05-13 14:00:16 +0000

    fixed user footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#index'
-
   get '/login', to: 'users#login', as: 'login'
   post '/login', to: 'users#in', as: 'in'
   resources :users, only: [:new,:create,:destroy]
   resources :recipes do
     resources :comments
   end
-
-
 
 
 end
