371e589865d5f54d9d21aa18d7fa3d81c656f1ad
illusioninlost <rchang9999@yahoo.com>
2018-06-26 22:23:27 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Recipe
+# t.string "name"
+# t.string "creator"
+# t.string "notes"
+# t.string "special_tools"
+# t.integer "approximate_time"
+# t.string "ingredients"
+# t.integer "user_id"
+
+Recipe.create(name: 'fried rice', creator:'A', notes: 'Add cooled rice onto a hot pan. Cook fast and thoroughly.', 
+     special_tools: 'wok, spatula', approximate_time: 20, ingredients: 'rice, sausage, peas, broccoli, carrots');
+Recipe.create(name: 'spaghetti', creator:'B', notes: 'Add noodels in pot and stir occasionally. Add with tomato sauce and additional toppings.', 
+     special_tools: 'spatula, fork-like spatula', approximate_time: 10, ingredients: 'hard noodles, tomato sauce, meatballs');
+Recipe.create(name: 'best chicken wings', creator:'C', notes: 'defrost wings and add seasoning(salt, pepper,onion, garlic, cheese), deep-fried until crispy. Double deep fried chicken wings for crispier texture.', 
+     special_tools: 'large bowl, tongs', approximate_time: 90, ingredients: 'raw chicken wings');
+Recipe.create(name: 'classic hamburger', creator:'D', notes: 'Toast buns; cut lettuce, slice tomotoes and onions and add to buns; grill salt and pepper beef; sandwich these with buns.', 
+     special_tools: 'grill,, cutting board', approximate_time: 60, ingredients: 'buns, lettuce, tomotoes, onions, beef');
